99e1f1d83595e410e881168cb74d81b6d993f4a3
Sasha <mr.mahahei.sasha@gmail.com>
2022-02-06 18:28:37 +0300

    problem2

--- src/components/StadiumDetails/index.js
+++ src/components/StadiumDetails/index.js
@@ -16,7 +16,7 @@ const StadiumDetails = () => {
 
 
 
-
+  const {id, name, players, gps} = details
 
 
   return (
@@ -24,7 +24,7 @@ const StadiumDetails = () => {
    {/* <div className="detail__button" onClick={() => onAcceptClick()}>Иду на футбик</div>
     <div className="details__name">{name}</div> */}
     {/* {playersList} */}
-   {/* <MapContainer center={[gps.latitude, gps.longitude]} zoom={14} scrollWheelZoom={true} className={"mapa"}>
+   <MapContainer center={[gps.latitude, gps.longitude]} zoom={14} scrollWheelZoom={true} className={"mapa"}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -33,7 +33,7 @@ const StadiumDetails = () => {
               <Circle center={[gps.latitude, gps.longitude]} radius={50} />
             </Marker>
           );
-      </MapContainer> */}
+      </MapContainer>
    </>
   );
 };
